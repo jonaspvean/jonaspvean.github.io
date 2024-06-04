4cf33b4281dd0f8691033049f4fee098c6372731
Jonas Vean <jonaspv@live.no>
2024-06-04 10:47:32 +0200

    tried to checkout _site from master to gh-pages

---
+++ _site/notes/quartz/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import custom from "./styles/custom.scss"
+
+
+interface Options {
+    links: Record<string, string>
+  }
+
+export default ((opts?: Options) => {
+    const NavBar: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+        
+    }
+})satisfies QuartzComponentConstructor
